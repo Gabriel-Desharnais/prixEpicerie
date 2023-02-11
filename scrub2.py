2a716be784a8e2e42675cf883e074f6877a4014c
gabriel-desharnais <gabriel.desharnais@hotmail.com>
2023-02-11 10:41:05 -0500

    syt

--- scrub2.py
+++ scrub2.py
@@ -3,9 +3,18 @@
 
 import pandas
 import maxi
+from pathlib import Path
 
 
 products = pandas.read_csv("productTable.csv")
 
+
 maxiProds = products.loc[products["maxi"] == True]
-print(maxi.scrub(maxiProds["uuid"]))
+infoReturned = maxi.scrub(maxiProds["uuid"])
+for uuid, info in infoReturned.items():
+	fi = [info["nom"], info["description"], info["marque"], info["format"]]
+	products.loc[products["uuid"] == uuid, ["nom","description","marque","Format"]] = fi
+	# TODO: ajouter une logique pour si il y a plus d'un uuid pareil
+
+filepath = Path("productTable.csv")
+products.to_csv(filepath, index=False)
