33a6b596954a9e87f008c2bf139a20265c8c6913
gabriel-desharnais <gabriel.desharnais@hotmail.com>
2023-02-21 11:42:23 -0500

    Ajouter le format

--- export.py
+++ export.py
@@ -16,19 +16,21 @@ rapport = wb.create_sheet("Prix",0)
 products = pandas.read_csv("productTable.csv")
 maxiProducts = pandas.read_csv("maxi/productMaxi.csv")
 
-productToExport = products.loc[products["maxi"] == True, ["uuid", "nom", "sku", "marque"]]
+productToExport = products.loc[products["maxi"] == True, ["uuid", "nom", "sku", "marque","Format"]]
 rapport.cell(1,1, "nom")
-rapport.cell(1,2, "marque")
-rapport.cell(1,3, "sku")
-rapport.cell(1,4, "prix")
-rapport.cell(1,5, "unité")
+rapport.cell(1,2, "format")
+rapport.cell(1,3, "marque")
+rapport.cell(1,4, "sku")
+rapport.cell(1,5, "prix")
+rapport.cell(1,6, "unité")
 for i, prod in enumerate(productToExport.values):
 	u = prod[0]
 	m = maxiProducts.loc[maxiProducts["uuid"] == u]
 	rapport.cell(i+2,1, prod[1])
-	rapport.cell(i+2,2, prod[3])
-	rapport.cell(i+2,3, prod[2])
-	rapport.cell(i+2,4, m["prix"].values[0])
-	rapport.cell(i+2,5, m["unité"].values[0])
+	rapport.cell(i+2,2, prod[4])
+	rapport.cell(i+2,3, prod[3])
+	rapport.cell(i+2,4, prod[2])
+	rapport.cell(i+2,5, m["prix"].values[0])
+	rapport.cell(i+2,6, m["unité"].values[0])
 
 wb.save("result/prix2023_02_21.xlsx")
